ed5a504b2db1f34446c4bc90c860d8e55e30e559
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2024-06-20 14:45:10 +0700

    Add Databased

---
+++ databased_src/timbangan_bayi.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 20, 2024 at 07:43 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `timbangan_bayi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `nama` varchar(100) NOT NULL,
+  `tb` varchar(10) NOT NULL,
+  `bb` varchar(10) NOT NULL,
+  `value` varchar(50) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `role` enum('Admin','User') NOT NULL DEFAULT 'User',
+  `active` enum('Yes','No') NOT NULL DEFAULT 'Yes'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `fullname`, `role`, `active`) VALUES
+('admin', '$2y$10$o4gI7IoI/U9LZzhH8Hd3m.goUQpaOVMD0l7xmkM4wSCyr87wvALm2', 'admin IoT', 'Admin', 'Yes'),
+('aku', '$2y$10$8Cm1ym2CTlO3vO0s5SB9su1BuhUUq2YaJRKeU56gRN/b7Ez11in1W', 'akuaja', 'Admin', 'Yes'),
+('user', '$2y$10$ZDY0vHBQ/IEyvaYQZN6eROVlx6jbw360Prj2IJL1qK68fyKTYDfrS', 'User IoT', 'User', 'Yes');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`nama`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
