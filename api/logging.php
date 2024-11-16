1a2555b562816d4b79fac42a053a1b0b0ddf89c6
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2024-11-16 19:53:09 +0700

    update api

--- api/logging.php
+++ api/logging.php
@@ -2,25 +2,29 @@
 include "../config/database.php";
 
 
-$webhookResponse = json_decode(file_get_contents('php://input'),true);
+$webhookResponse = json_decode(json: file_get_contents(filename: 'php://input'),associative: true);
 $topic = $webhookResponse["topic"];
-$ypaload = $webhookResponse["payload"];
+$payload = $webhookResponse["payload"];
 
-$topicExplode = explode("/", $topic);
-
-$serialNumber = $topicExplode[1];
-$name = $topicExplode[2];
-
-if($topicExplode[2] == "suhu" || $topicExplode[2] == "kelembaban" || $topicExplode[2] == "potentiometer"){
-    $type = "sensor";
-} else {
-    $type = "actuator";
-}
-
-$sql = "INSERT INTO data (serial_number, sensor_actuator, value, name, mqtt_topic)
-        VALUES ('$serialNumber', '$type', '$payload', '$name', '$topic')";
-
-mysqli_query($conn, $sql);
+$topicExplode = explode(separator: "/", string: $topic);
+$payloadExplode = explode(separator: "/", string: $payload);
 
 
+$nama = $payloadExplode[0];
+$tb = $payloadExplode[1];
+$bb = $payloadExplode[2];
+$value = $payloadExplode[3];
+
+// if($topicExplode[2] == "suhu" || $topicExplode[2] == "kelembaban" || $topicExplode[2] == "potentiometer"){
+//     $type = "sensor";
+// } else {
+//     $type = "actuator";
+// }
+
+$sql = "INSERT INTO data (nama, tb, bb, value)
+        VALUES ('$nama', '$tb', '$bb', '$value')";
+
+mysqli_query( $conn,$sql);
+
+// echo $sql;
 ?>
--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "timbangan";
+$database = "timbangan_bayi";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
